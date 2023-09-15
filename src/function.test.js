ef4a56f811935e95d032c527254784ee2c304856
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-09-15 17:21:37 -0400

    added jest

---
+++ src/function.test.js
@@ -0,0 +1,20 @@
+
+import {
+    capitalize,
+    reverse,
+  } from "./index";
+  
+  
+  
+  
+  //capitalize string test
+  
+  test('check if first letter is capital', () => {
+    expect(capitalize('hello')).toBe('Hello');
+  });
+  
+  //reverse string test
+  
+  test('check if string is reversed', () => {
+    expect(reverse('real')).toBe('laer');
+  });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import './styles.css';
+//change later when want to use css ******
+
+//import './styles.css';
+
+//capitalize string
+function capitalize(string) {
+
+    return string[0].toUpperCase() + string.slice(1)
+
+}
 
 
+//reverse string
+function reverse(string) {
 
-console.log('hi')
+    let arr = []
+
+    for (let i = string.length; i >= 0; i--) {
+        arr.push(string[i])
+    }
+    return arr.join("")
+}
+
+export {
+    capitalize,
+    reverse,
+    
+};
