f79d87e3ead5e0f57b122169f476bb6d28597c46
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-09-28 13:29:57 -0400

    added file

--- src/dom.js
+++ src/dom.js
@@ -2,12 +2,11 @@
 
 function createPlayerGrid (array) {
     
-console.log(array.board[0].length)
 
     for (let i=0; i<array.board.length; i++) {
 
         for(let y=0; y<array.board[i].length; y++) {
-            console.log(y)
+            
          const container = document.querySelector('#container');
          const grid = document.createElement('div');
         
@@ -20,8 +19,27 @@ console.log(array.board[0].length)
     
     }
 
+    function createComputerGrid (array) {
+    
+
+        for (let i=0; i<array.board.length; i++) {
+    
+            for(let y=0; y<array.board[i].length; y++) {
+                
+             const container = document.querySelector('#container');
+             const grid = document.createElement('div');
+            
+             grid.classList.add('grid');
+             
+             container.appendChild(grid);
+            }
+        
+        }
+        
+        }
+
     export {
         createPlayerGrid,
-    
+        createComputerGrid,
     };
 
