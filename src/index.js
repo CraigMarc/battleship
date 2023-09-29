298abc260b6339e5c94d02ea2a2d7018fdcc7b1a
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-09-29 14:06:21 -0400

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -119,20 +119,13 @@ function placePlayerShips() {
 
 }
 
-
-
-
 createComputerGrid(computerBoard)
 
 //create player board
 let playerBoard = new Board
-
-
-//computerPlayer(playerBoard)
-//playerBoard.attack(0, 0)
 createPlayerGrid(playerBoard)
 
-//add eventlistener for player
+//add player round function
 
 function playRound(board) {
 
@@ -240,7 +233,6 @@ function newGame() {
     cBattleship = new Ship(4)
     cSub = new Ship(3)
     cPatrol = new Ship(2)
-
     computerBoard = new Board
     placeComputerShips()
     createComputerGrid(computerBoard)
@@ -252,55 +244,11 @@ function newGame() {
 }
 
 
-
-
-//******* get rid of later */
+//call button and change board functs
 buttons()
-
 changeBoard()
 
-//playRound(computerBoard)
-
-
-// play round 
-/*
-function playRound() {
-
-  player(computerBoard)
-  computerPlayer(playerBoard)
-  removeBoard()
-  createComputerGrid(computerBoard)
-  createPlayerGrid(playerBoard)
-
-}
-
-playRound()
-//
-
-/*
-//destroyer.hit(3)
-//destroyer.hit(4)
-
-console.log(destroyer)
-  //console.log(destroyer.sunk())
-
-//let carrier = new Ship(6)
 
 
 
-let board1 = new Board
-board1.placeShip(0, 0, destroyer, 'v')
-//board1.placeShip(3, 2, patrol, 'h')
 
-//console.log(board1.checkPlacement(1, 0, patrol, 'h'))
-
-
-//board1.attack(5,6)
-//board1.attack(0,1)
-//board1.attack(0,0)
-board1.attack(3,2)
-
-console.log(destroyer)
-//console.log(patrol)
-
-console.log(board1)*/
